c613eab34c92f37880fa70f8e6dac8143e40c429
Thedarkplays <64837648+Thedarkplays@users.noreply.github.com>
2024-06-29 22:41:44 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,32 +1,26 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# Install Chrome
-if [[ ! -d /opt/render/project/.render/chrome ]]; then
-  echo "...Downloading Chrome"
-  mkdir -p /opt/render/project/.render/chrome
-  wget -P /opt/render/project/.render/chrome https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x /opt/render/project/.render/chrome/google-chrome-stable_current_amd64.deb /opt/render/project/.render/chrome
-  rm /opt/render/project/.render/chrome/google-chrome-stable_current_amd64.deb
-else
-  echo "...Using Chrome from cache"
+CHROME_PATH=/opt/render/project/bin/chrome/opt/google/chrome/
+
+if [[ ! -d $CHROME_PATH ]]; then
+    echo "...Downloading Chrome Binary..."
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /tmp
+    echo "...Installing Chrome Binary..."
+    mkdir -p /opt/render/project/bin/chrome
+    dpkg -x /tmp/google-chrome-stable_current_amd64.deb /opt/render/project/bin/chrome
+    rm /tmp/google-chrome-stable_current_amd64.deb
+    export PATH="${PATH}:${CHROME_PATH}"
 fi
 
-# Install ChromeDriver
-if [[ ! -f /opt/render/project/.render/chromedriver ]]; then
-  echo "...Downloading ChromeDriver"
-  wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
-  unzip /tmp/chromedriver.zip chromedriver -d /opt/render/project/.render
-  rm /tmp/chromedriver.zip
-else
-  echo "...Using ChromeDriver from cache"
+CHROMEDRIVER_PATH=/opt/render/project/bin/chromedriver
+
+if [[ ! -d $CHROMEDRIVER_PATH ]]; then
+    echo "...Downloading Chromedriver..."
+    wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+    unzip /tmp/chromedriver.zip chromedriver -d /opt/render/project/bin
+    rm /tmp/chromedriver.zip
+    export PATH="${PATH}:${CHROMEDRIVER_PATH}"
 fi
 
-# Add Chrome to PATH
-export PATH="/opt/render/project/.render/chrome/opt/google/chrome:$PATH"
-export CHROME_PATH="/opt/render/project/.render/chrome/opt/google/chrome"
-export CHROMEDRIVER_PATH="/opt/render/project/.render/chromedriver"
-
-# Install Python dependencies
 pip install -r requirements.txt
